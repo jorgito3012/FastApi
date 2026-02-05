4bfeed58b5c2458249cba5fd6384b9fc049b2456
Jorge Narbona Gallego <jorgenarbona10@gmail.com>
2026-02-05 16:46:56 +0100

    Cambios para el ejercicio 5 para que funcione

--- src/data/db.py
+++ src/data/db.py
@@ -9,7 +9,7 @@ db_port: int = int(os.getenv("DB_PORT", 5432))
 db_name: str = os.getenv("DB_NAME", "pokemonsdb")
 
 DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
-engine = create_engine("DB_URL", DATABASE_URL, echo=True)
+engine = create_engine(os.getenv("DB_URL", DATABASE_URL), echo=True)
 
 def get_session():
     with Session(engine) as session:
